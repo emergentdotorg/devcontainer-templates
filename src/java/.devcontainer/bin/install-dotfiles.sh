150ee2b81f9a2d9dfdfb88672cef43dfb3825a0d
Patrick Woodworth <patrick@woodworth.org>
2025-01-22 00:28:03 -0800

    More template updates

--- src/java/.devcontainer/bin/install-dotfiles.sh
+++ src/java/.devcontainer/bin/install-dotfiles.sh
@@ -7,7 +7,16 @@ set -eu
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
 
-DOTFILES_REPO="${DOTFILES_REPO:-pwoodworth/idea-devcontainers-dotfiles}"
+
+#[ -z "$( git config --get user.email )" ] && [ -n "${GIT_USER_EMAIL}" ] && git config --global user.email "${GIT_USER_EMAIL}"
+#[ -z "$( git config --get user.name )" ] && [ -n "${GIT_USER_NAME}" ] && git config --global user.name "${GIT_USER_NAME}"
+
+if [ -z "${DOTFILES_REPO}" ]; then
+  echo "DOTFILES_REPO not set!"
+  exit 0
+fi
+
+DOTFILES_REPO="${DOTFILES_REPO:?DOTFILES_REPO empty or unset}"
 DOTFILES_DEST="${DOTFILES_DEST:-${HOME}/.dotfiles}"
 DOTFILES_INIT="${DOTFILES_INIT:-install.sh}"
 
